57f1e293a44192fed405a7f8467f5f4626e9261f
Arnoldas nesvarbu <arnasmoz@gmail.com>
2025-03-21 21:54:55 +0200

    display leddit rows

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
-import { useState } from 'react';
+import { use, useState } from 'react';
 import './App.css';
 
 function App() {
   const [inputText, setInputText] = useState('');
-  const [tableData, setTableData] = useState([
-    { id: 0, upvotes: 60, nsfw: false, title: 'Item 1', length: '07:12' },
-    { id: 1, upvotes: 67, nsfw: true, title: 'Item 2', length: '00:13' },
-  ]);
+  const [tableData, setTableData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setErrors] = useState(null);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [selectAll, setSelectAll] = useState(false);
 
   const handleInputChange = (e) => 
   {
     setInputText(e.target.value);
+  }
+
+  const handleDownload = async () =>{
+    
+  }
+
+  const handleSearchClick = async () =>
+  {
+    setIsLoading(true);
+    try{
+        const response = await fetch('https://localhost:7032/Scraper?subreddit='+inputText, 
+        {
+          method: 'GET',
+          headers: {
+                'Content-Type': 'application/json',
+          },
+        });
+
+        if(!response.ok)
+        {
+           throw new Error('Failed to fetch');
+        }
+        const data = await response.json();
+        setTableData(data);
+        setErrors(null);
+    }
+    catch(err){
+      setErrors('Error: '+ err.message);
+      console.error('Error: ', err);
+    }
+    finally{
+      setIsLoading(false);
+    }
+  }
+
+  const handleSelectItem = (id) => {
+    setSelectedItems({
+      ...selectedItems,
+      [id]: !selectedItems[id]
+    });
+
+    if(selectAll && selectedItems[id] == true)
+    {
+      setSelectAll(false);
+    }
+  }
+
+  const handleSelectAll = () => {
+    const newSelectAll = !selectAll;
+    setSelectAll(newSelectAll);
+
+    const newSelectedItems = {};
+    tableData.forEach(item => {
+      newSelectedItems[item.id] = newSelectAll;
+    });
+    setSelectedItems(newSelectedItems);
   }
 
   return (
@@ -24,40 +81,75 @@ function App() {
       <div className="content">
         <h1>Input subreddit</h1>
 
+        {error && <div className="error-message">{error}</div>}
+
         <div className="input-container">
           <input type="text"
           value={inputText}
           onChange={handleInputChange}
           placeholder="Enter subreddit here"/>
-        <button>Scrape</button>
+
+        <button
+        onClick={handleSearchClick}
+        disabled={isLoading}>
+          {isLoading ? 'Scraping' : 'Scrape'}
+        </button>
+
+        <button
+          onClick={handleDownload}
+          disabled={isLoading}>
+          Download selected
+        </button>
         </div>
 
         <div className="table-container">
+          {isLoading && !tableData.length ? (
+            <div className="loading">Loading data...</div>
+          ) : (
           <table>
             <thead>
             <tr>
+              <th className="select-column">
+                <input
+                type="checkbox"
+                checked={selectAll}
+                onChange={handleSelectAll}/>
+              </th>
               <th>Upvotes</th>
               <th>NSFW</th>
               <th>Title</th>
-              <th>Length</th>
+              <th>Length (seconds)</th>
             </tr>
             </thead>
           <tbody>
-            {tableData.sort(f => f.upvotes).map((item) => (
-              <tr key={item.id}>
-                <td>{item.upvotes}</td>
+            {tableData.sort(f => f.upvoteRatio).map((item) => (
+              <tr key={item.id} className={selectedItems[item.id] ? "selected-row" : ""}>
+                <td hidden>{item.downloadLink}</td>
+                <td className="select-column">
+                  <input
+                  type="checkbox"
+                  checked={selectedItems[item.id] || false}
+                  onChange={() => handleSelectItem(item.id)}/>
+                </td>
+                <td>{item.upvoteRatio}</td>
                 <td className="checkbox-cell">
                   <input
                   disabled
                   type="checkbox"
-                  checked={item.nsfw}/>
+                  checked={item.isnsfw}/>
                 </td>
                 <td>{item.title}</td>
-                <td>{item.length}</td>
+                <td>{item.mediaLength}</td>
               </tr>
             ))}
+            {isLoading && tableData.length === 0 && (
+               <tr>
+               <td colSpan="4" className="no-data">No items found</td>
+             </tr>
+            )}
           </tbody>
           </table>
+          )}
         </div>
       </div>
     </div>
